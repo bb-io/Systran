edb37422eb5803eacb73e7c1cbf6fa2ca1047961
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-07 23:14:26 +0200

    fix

--- Apps.Systran/Models/Response/TranslateTextResponse.cs
+++ Apps.Systran/Models/Response/TranslateTextResponse.cs
@@ -1,43 +1,14 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 
 namespace Apps.Systran.Models.Response
 {
     public class TranslateTextResponse
     {
-        [Display("Request ID")]
-        public string RequestId { get; set; } = string.Empty;
-
-        public List<TranslationOutput> Outputs { get; set; } = new();
-    }
-
-    public class TranslationOutput
-    {
-        [Display("Info")]
-        public TranslationInfo? Info { get; set; }
-
         [Display("Translated text")]
+        [JsonProperty("output")]
         public string Output { get; set; } = string.Empty;
-
-        [Display("Back translation")]
-        public string? BackTranslation { get; set; }
-
-        [Display("Source text")]
-        public string? Source { get; set; }
     }
 
-    public class TranslationInfo
-    {
-        [Display("Language identification")]
-        public LanguageIdentification? Lid { get; set; }
-
-    }
-
-    public class LanguageIdentification
-    {
-        [Display("Language")]
-        public string Language { get; set; } = string.Empty;
-
-        [Display("Confidence level")]
-        public float Confidence { get; set; }
-    }
+  
 }
