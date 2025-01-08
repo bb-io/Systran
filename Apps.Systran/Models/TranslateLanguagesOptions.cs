becbaa03986c4c43e6bbfa93f407b52f2b0a774b
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-08 12:21:17 +0200

    fixed language  provider

--- Apps.Systran/Models/TranslateLanguagesOptions.cs
+++ Apps.Systran/Models/TranslateLanguagesOptions.cs
@@ -2,17 +2,18 @@
 using Apps.Systran.DataSourceHandlers.EnumDataHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Systran.Models
 {
     public class TranslateLanguagesOptions
     {
         [Display("Source language code")]
-        [StaticDataSource(typeof(SupportedLanguagesDataHandler))]
+        [DataSource(typeof(SupportedLanguagesDataHandler))]
         public string Source { get; set; }
 
         [Display("Target language code")]
-        [StaticDataSource(typeof(SupportedLanguagesDataHandler))]
+        [DataSource(typeof(SupportedLanguagesDataHandler))]
         public string Target { get; set; }
     }
 }
